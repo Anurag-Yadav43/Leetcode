7c1a9198bb9ef3ad537fc05126438d86bc95b153
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-02-26 22:28:46 +0530

    Create file

---
+++ 1749-MaximumAbsoluteSumofAnySubarray/1749-MaximumAbsoluteSumofAnySubarray.java
@@ -0,0 +1,25 @@
+class Solution {
+     private int maxSumSubarrayKadanes(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int num : nums) {
+            currSum += num;
+            maxSum = Math.max(maxSum, currSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+    public int maxAbsoluteSum(int[] nums) {
+        int maxSumSubarray = maxSumSubarrayKadanes(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = -nums[i];
+        }
+
+        int minSumSubarray = maxSumSubarrayKadanes(nums);
+        return Math.max(maxSumSubarray, minSumSubarray);
+
+    }
+}
