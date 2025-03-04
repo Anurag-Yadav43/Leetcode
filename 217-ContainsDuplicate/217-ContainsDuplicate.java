52fc936b762148cdda544adb5e66129558be7c8a
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-03-04 22:50:11 +0530

    File updated at 3/4/2025, 10:50:11 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
+        HashSet<Integer> hashset = new HashSet<>();
+        for(int num: nums){
+            if(hashset.contains(num)){
                 return true;
             }
+            hashset.add(num);
         }
         return false;
     }
