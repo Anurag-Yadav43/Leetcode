e7a7b7a868c65d8212cd6b089f4ffc83d6b4170c
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-03-05 01:01:10 +0530

    File updated at 3/5/2025, 1:01:09 AM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,6 +1,23 @@
 class Solution {
     public int findMin(int[] nums) {
-        Arrays.sort(nums);
-        return nums[0];
+        // Arrays.sort(nums);
+        // return nums[0];
+        //o(nlogn)
+
+        //use binary search for O(n)
+
+        int left =0, right = nums.length-1;
+        while(left<right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid] > nums[right]){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+
+        return nums[left];
     }
 }
