ff0d4534f271065879cdde028b4f4823ad931e0b
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-03-05 23:35:03 +0530

    File updated at 3/5/2025, 11:35:01 PM

--- 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.java
+++ 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.java
@@ -1,5 +1,15 @@
 class Solution {
     public long coloredCells(int n) {
-        return 1 + 4 * ((long) n * (n - 1) / 2);
+        //return 1 + 4 * ((long) n * (n - 1) / 2); solutin here
+
+        //another solution
+         long count = 1;
+        long jumpSize = 4;
+        while (n > 1) {
+            count += jumpSize;
+            jumpSize += 4;
+            n--;
+        }
+        return count;
     }
 }
