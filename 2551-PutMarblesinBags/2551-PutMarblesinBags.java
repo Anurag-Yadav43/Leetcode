9f1922f8b26095278aa080cc6e6e11ab6a8673dc
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-03-31 23:01:14 +0530

    Create file

---
+++ 2551-PutMarblesinBags/2551-PutMarblesinBags.java
@@ -0,0 +1,23 @@
+// Last updated: 3/31/2025, 11:01:12 PM
+class Solution {
+    public long putMarbles(int[] weights, int k) {
+         int n = weights.length;
+        
+        List<Integer> pairSum = new ArrayList<>();
+        
+        for (int i = 0; i < n - 1; i++) {
+            pairSum.add(weights[i] + weights[i + 1]);
+        }
+        
+        Collections.sort(pairSum);
+        long maxSum = 0;
+        long minSum = 0;
+        
+        for (int i = 0; i < k - 1; i++) {
+            minSum += pairSum.get(i);
+            maxSum += pairSum.get(n - i - 2);
+        }
+        
+        return maxSum - minSum;
+    }
+}
