d6c57f5863a37299de2b1ae534141769f49bff2e
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-04-18 23:06:36 +0530

    File updated at 4/18/2025, 11:06:35 PM

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,21 +1,24 @@
+// Last updated: 4/18/2025, 11:06:35 PM
 class Solution {
+
     private List<List<Integer>> result = new ArrayList<>();
 
     public List<List<Integer>> subsets(int[] nums) {
         List<Integer> temp = new ArrayList<>();
         solve(nums, 0, temp);
         return result;
-    }
 
-    private void solve(int[] nums, int idx, List<Integer> temp) {
-        if (idx >= nums.length) {
+    }
+    private void solve(int[] nums,int index,List<Integer> temp){
+        if (index >= nums.length) {
             result.add(new ArrayList<>(temp));
             return;
         }
 
-        temp.add(nums[idx]);
-        solve(nums, idx + 1, temp);
+        temp.add(nums[index]);
+        solve(nums, index + 1, temp);
         temp.remove(temp.size() - 1);
-        solve(nums, idx + 1, temp);
+        solve(nums, index + 1, temp);
     }
+
 }
