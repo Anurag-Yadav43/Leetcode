fc78d22cefb3bbca3fa3a6f987d5e75860daebbe
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-04-19 20:58:33 +0530

    File updated at 4/19/2025, 8:58:31 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,32 +1,51 @@
-// Last updated: 3/26/2025, 9:08:46 PM
+// Last updated: 4/19/2025, 8:58:31 PM
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int index[]  = new int[2];
-        int count =0;
+        int []arr = new int[2];
+        arr[0] = findFirst(nums,target);
+        arr[1] = findLast(nums,target);
 
-        if(nums.length == 0){
-            index[0] = -1;
-            index[1] = -1;
-        }
+        return arr;
         
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                count++;
-            }
-        }
+    }
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                index[0] = i;
-                index[1] = i+(count-1);
-                break;
+    private int findFirst(int []nums,int target){
+        int start = 0, end = nums.length-1;
+        int index = -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
+                index = mid;
+                end = mid-1;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
             }
             else{
-                index[0] = -1;
-                index[1] = -1;
+                end = mid - 1;
             }
         }
+        return index;
+    }
 
+    private int findLast(int []nums,int target){
+        int start = 0, end = nums.length-1;
+        int index = -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
+                index = mid;
+                start = mid + 1;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
         return index;
     }
 }
