768228060b2c3dee4a890e4ce4386e62197ddc5b
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-04-21 01:06:05 +0530

    File updated at 4/21/2025, 1:06:05 AM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
@@ -1,12 +1,24 @@
-// Last updated: 4/21/2025, 12:58:46 AM
+// Last updated: 4/21/2025, 1:06:05 AM
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] res = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            res[i] = nums[i] * nums[i];
-        }
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 0, right = n - 1;
+        int pos = n - 1;
 
-        Arrays.sort(res);
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                res[pos] = leftSquare;
+                left++;
+            } else {
+                res[pos] = rightSquare;
+                right--;
+            }
+            pos--;
+        }
 
         return res;
     }
