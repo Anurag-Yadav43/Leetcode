ed59d82dbf75fa8fe68e800f3ba1aa2acb3a5b71
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-04-28 20:56:04 +0530

    Create file

---
+++ 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.java
@@ -0,0 +1,26 @@
+// Last updated: 4/28/2025, 8:56:02 PM
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        int n = nums.length;
+        
+        long result = 0;
+        long sum = 0;
+        
+        int i = 0;
+        int j = 0;
+        
+        while (j < n) {
+            sum += nums[j];
+            
+            while (i <= j && sum * (j - i + 1) >= k) {
+                sum -= nums[i];
+                i++;
+            }
+            
+            result += (j - i + 1);
+            j++;
+        }
+        
+        return result;
+    }
+}
