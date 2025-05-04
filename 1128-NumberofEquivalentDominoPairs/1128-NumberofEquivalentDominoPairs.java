c7a88864c4f5ea23acb9d051928231c5191c38c1
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-05-04 15:36:13 +0530

    Create file

---
+++ 1128-NumberofEquivalentDominoPairs/1128-NumberofEquivalentDominoPairs.java
@@ -0,0 +1,21 @@
+// Last updated: 5/4/2025, 3:36:10 PM
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] count = new int[100];
+        int result = 0;
+
+        for (int[] d : dominoes) {
+            int a = d[0], b = d[1];
+            if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            int num = a * 10 + b;
+            result += count[num];
+            count[num]++;
+        }
+
+        return result;
+    }
+}
