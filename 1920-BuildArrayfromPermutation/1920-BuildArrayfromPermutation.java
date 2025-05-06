8bb7d44484db705cac00e3a97d18733ce389a6ae
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-05-06 21:01:13 +0530

    File updated at 5/6/2025, 9:01:12 PM

--- 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.java
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.java
@@ -1,10 +1,13 @@
-// Last updated: 5/6/2025, 8:10:13 PM
+// Last updated: 5/6/2025, 9:01:12 PM
 class Solution {
     public int[] buildArray(int[] nums) {
-        int []arr = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            arr[i] = nums[nums[i]];
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            nums[i] = nums[i] + n*(nums[nums[i]]%n);
         }
-        return arr;
+        for(int i=0;i<n;i++){
+            nums[i] = nums[i]/n;
+        }
+        return nums;
     }
 }
