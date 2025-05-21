80b20e0e59d7ba4093d16a9cb3970868ac978ef1
Anurag Yadav <120739011+Anurag-Yadav43@users.noreply.github.com>
2025-05-22 01:41:55 +0530

    File updated at 5/22/2025, 1:41:54 AM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,6 +1,62 @@
-// Last updated: 5/22/2025, 1:19:56 AM
+// Last updated: 5/22/2025, 1:41:54 AM
 class Solution {
+    public void setZeroes(int[][] matrix){
+        //Approach-3 (In place constant space)
+        //T.C : O(m*n)
+        //S.C : O(1)
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check first column separately
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) 
+                firstColZero = true;
+        }
+
+        // Check first row separately
+        for(int j = 0; j < n; j++) {
+            if(matrix[0][j] == 0) 
+                firstRowZero = true;
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstRowZero) {
+            for(int j = 0; j < n; j++) 
+                matrix[0][j] = 0;
+        }
+
+        if(firstColZero) {
+            for(int i = 0; i < m; i++) 
+                matrix[i][0] = 0;
+        }
+    }
+
+}
+    /*
     public void setZeroes(int[][] matrix) {
+        //better approach
+        //Approch-2 (Using m+n extra space)
+        //T.C : O(m*n)
+        //S.C : O(m+n)
         int m = matrix.length;
         int n = matrix[0].length;
 
@@ -24,14 +80,16 @@ class Solution {
             }
         }
     }
-}
+    */
+
 
 /*
 //brute force
     //Approch-1 (Using extra space of m*n)
     //T.C : O(m*n*(m+n))
     //S.C : O(m*n)
-int m = matrix.length;
+    public void setZeroes(int[][] matrix){
+        int m = matrix.length;
         int n = matrix[0].length;
         int[][] result = new int[m][n];
         for(int i=0;i<m;i++){
